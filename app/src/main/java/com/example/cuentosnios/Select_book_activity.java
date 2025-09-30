cd4d8d63db660ec85119e8111d0481d30616c4b3
Florencia Figueroa <165704067+fl0rfigueroa@users.noreply.github.com>
2025-09-30 16:37:29 -0300

    feat: posicionar header y carrusel con constraint

--- app/src/main/java/com/example/cuentosnios/Select_book_activity.java
+++ app/src/main/java/com/example/cuentosnios/Select_book_activity.java
@@ -8,13 +8,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoadingActivity extends AppCompatActivity {
+public class Select_book_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.select_book_activity);
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
